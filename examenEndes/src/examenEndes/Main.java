c16b7d3b5c20b6f7f491f0ec9c1f5435dbfefc53
May <May@DESKTOP-0MUMB30>
2022-12-07 19:43:48 +0100

    añadido mensaje despedida

--- examenEndes/src/examenEndes/Main.java
+++ examenEndes/src/examenEndes/Main.java
@@ -3,7 +3,9 @@ package examenEndes;
 public class Main {
 
 	public static void main(String[] args) {
-	System.out.println("hola me llamo");
+	System.out.println("hola me llamo May");
+	
+	System.out.println("hasta luego");
 	}
 
 }
